e3d361def66c1650936ce1dca0ae0c6820b1a0b9
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-29 14:50:13 +0300

    navbar added

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Home } from './components/Home';
 import { Menu } from './components/Menu';
 import { About } from './components/About';
 import { Contact } from './components/Contact';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/menu" exact element={<Menu />} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Burgerlogo from "../assets/burgerlogo.png";
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+        <div className='main'>
+            <img src={Burgerlogo} alt="" />
+        <div>
+            <Link to="/">Anasayfa</Link>
+            <Link to="/menu">Menü</Link>
+            <Link to="/about">Hakkımızda</Link>
+            <Link to="/contact">İletişim</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar
