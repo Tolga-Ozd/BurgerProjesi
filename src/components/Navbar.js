b9156d67276dd052e2781903e8d3815ef7335c93
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-29 15:01:26 +0300

    css added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Burgerlogo from "../assets/burgerlogo.png";
 import { Link } from 'react-router-dom';
+import "../styles/Navbar.css"
 
 const Navbar = () => {
   return (
     <div className='navbar'>
         <div className='main'>
             <img src={Burgerlogo} alt="" />
-        <div>
+        <div className='mainLink'>
             <Link to="/">Anasayfa</Link>
             <Link to="/menu">Menü</Link>
             <Link to="/about">Hakkımızda</Link>
