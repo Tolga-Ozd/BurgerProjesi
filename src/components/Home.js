b75d1d4794ad12b8b6fbe4e563ce781fc9b7a507
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-30 13:05:41 +0300

    home page added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,18 @@
 import React from 'react';
+import { Link } from "react-router-dom";
+import BannerImage from "../assets/banneryeni.jpg";
+import "../styles/Home.css";
 
 export const Home = () => {
-  return <div>Home</div>;
+  return (
+
+  <div className='mainPage' style={{backgroundImage:`url(${BannerImage})`}}>
+    <div className='order'> 
+       <Link to ="/menu" >
+          <button >SİPARİŞ VER</button>
+       </Link> 
+    </div>
+  </div>
+
+  );
 };
