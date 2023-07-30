6f9cd2ef3c7384529cb6e6fca0bca9faf443c69a
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-30 15:22:26 +0300

    menu added

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,28 @@
+import  MenuItem  from '../components/MenuItem';
 import React from 'react';
+import {Data} from "../helpers/Data" ;
+import "../styles/Menu.css";
+
 
 export const Menu = () => {
-  return <div>Menu</div>;
+  return( 
+  <div className='menu'>
+      <h1 className='menuTitle'>Köftelerimiz</h1>
+      <div className='menuList'>
+          {Data.map((menuItem , key)=>{
+              return(
+                  <MenuItem 
+                  key ={key}
+                  image = {menuItem.image}
+                  name = {menuItem.image}
+                  content = {menuItem.content}
+                  price = {menuItem.price}
+
+                />
+              ); 
+         })}
+      </div>
+  </div>
+  );
 };
+ 
---
+++ src/helpers/Data.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import BurgerImage from   "../assets/burger.jpg";
+
+export const Data = [
+    {
+        name:"Efso Burger",
+        image: BurgerImage,
+        content : "150 gr kasap Burger , patates , turşu",
+        price: 250 , 
+    },
+    {
+        name:"Efso Burger",
+        image: BurgerImage,
+        content : "150 gr kasap Burger , patates , turşu",
+        price: 250 , 
+    },
+    {
+        name:"Efso Burger",
+        image: BurgerImage,
+        content : "150 gr kasap Burger , patates , turşu",
+        price: 250 , 
+    },
+    {
+        name:"Efso Burger",
+        image: BurgerImage,
+        content : "150 gr kasap Burger , patates , turşu",
+        price: 250 , 
+    },
+    {
+        name:"Efso Burger",
+        image: BurgerImage,
+        content : "150 gr kasap Burger , patates , turşu",
+        price: 250 , 
+    },
+    {
+        name:"Efso Burger",
+        image: BurgerImage,
+        content : "150 gr kasap Burger , patates , turşu",
+        price: 250 , 
+    },
+]
+
+
