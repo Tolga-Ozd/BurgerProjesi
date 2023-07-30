842eb540eeb4beb3111905b9b27ed0a6642f52e3
Tolga ÖZDİLEK <ozdilektolga7140@gmail.com>
2023-07-30 20:46:27 +0300

    add

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import BannerImage from "../assets/banneryeni.jpg";
+import "../styles/About.css";
 
 export const About = () => {
-  return <div>About</div>;
+  return(
+    <div className='about'>
+      <div className='aboutTop' style={{backgroundImage: `url(${BannerImage})`}}></div>
+
+      <div className='aboutBottom'>
+        <h1>Hakkımızda</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum magni cupiditate in nulla, est dolores praesentium quis, necessitatibus, ab delectus doloribus consectetur repellendus quia quidem a. Dolorum exercitationem deserunt eveniet.</p>
+      </div>
+   </div>
+  );
 };
